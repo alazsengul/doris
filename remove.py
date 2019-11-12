6738f4d15ff23578afa1cb7de871e54d73c1afb4
Alaz Sengul <alazsengul@gmail.com>
2019-11-12 00:19:58 -0500

    Algorithm works

--- remove.py
+++ remove.py
@@ -2,20 +2,69 @@ import os
 import PIL
 import pdf2image
 from tqdm import tqdm
+from collections import defaultdict
 
 # dots per inch (dpi) adjusts quality and file size
-
 images = pdf2image.convert_from_path('files/lurie.pdf', dpi = 300)
 
-for im in tqdm(images, desc = "Images: "):
+# figure out watermark colors
+im = images[0]
 
-    im_width, im_height = im.size
-    pixels = im.load()
+im_width, im_height = im.size
+pixels = im.load()
 
-    for w in tqdm(range(1, im_width), desc = "Pixels: "):
-        for h in range(1, im_height):
+colors_to_freq = defaultdict(int)
 
-            if im.getpixel((w, h)) != (0, 0, 0):
-                pixels[w, h] = (255, 255, 255)
+for y in range(im_height):
 
-images[0].save("files/out.pdf", save_all = True, append_images = images[1:], subsampling=0, quality=100)
+    line_pixels = set()
+
+    contains_black = False
+
+    for x in range(im_width):
+
+        current_pixel = im.getpixel((x, y))
+
+        if current_pixel == (0, 0, 0):
+            contains_black = True
+            break
+        else:
+            line_pixels.add(current_pixel)
+
+    if not contains_black:
+        no_white = line_pixels - set([(255, 255, 255)])
+
+        if no_white:
+            colors_to_freq[tuple(list(no_white))] += 1
+
+watermark_colors = set(list(sorted(colors_to_freq, key = colors_to_freq.get, reverse = True)[0]))
+
+print(watermark_colors)
+
+
+
+
+for y in range(im_height):
+
+    for x in range(im_width):
+
+        if im.getpixel((x, y)) in watermark_colors:
+            pixels[x, y] = (255, 255, 255)
+
+
+im.save("files/out.jpg", subsampling=0, quality=100)
+
+
+
+
+
+
+
+
+# for im in tqdm(images[:1], desc = "Images: "):
+
+
+            #if im.getpixel((w, h)) != (0, 0, 0):
+            #    pixels[w, h] = (255, 255, 255)
+
+# images[0].save("files/out.pdf", save_all = True, append_images = images[1:], subsampling=0, quality=100)
